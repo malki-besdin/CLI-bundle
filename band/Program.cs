48f15ec95059880f500dc8fa5b74f0fa6694035f
malki-besdin <m556738087@gmail.com>
2024-03-30 23:49:30 +0300

    CLIbundle

---
+++ band/Program.cs
@@ -0,0 +1,22 @@
+﻿//band bundel --output D:\folder\band.txt
+using System.CommandLine;
+
+var bundleOption = new Option<FileInfo>("--output", "File path and name");
+var bundleCommand = new Command("bundel", "Bundel code files to a single file");
+bundleCommand.AddOption(bundleOption);
+bundleCommand.SetHandler((output) =>
+{
+    try
+    {
+        File.Create(output.FullName);
+        Console.WriteLine("File is creating!!!!");
+    }
+    catch (DirectoryNotFoundException ex)
+    {
+        Console.WriteLine("Error: file path is invalid"+ex);
+    }
+}, bundleOption);
+
+var rootCommand = new RootCommand("Root command File Bundler CLI");
+rootCommand.AddCommand(bundleCommand);
+rootCommand.InvokeAsync(args);
